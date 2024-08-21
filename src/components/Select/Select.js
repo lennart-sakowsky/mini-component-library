2b232f6d356367c54231df585275ab21537ff797
Lennart Sakowsky <lennart.sakowsky@ergosign.de>
2024-08-21 11:55:54 +0200

    Josh's Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -10,43 +10,61 @@ const Select = ({ label, value, onChange, children }) => {
 
   return (
     <Wrapper>
-      <SelectElement value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </SelectElement>
-      <SelectIcon id="chevron-down" size={24} strokeWidth={2} />
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   position: relative;
-  width: min-content;
+  width: max-content;
 `;
 
-const SelectElement = styled.select`
-  padding: 12px 40px 12px 16px;
-  font-size: 1rem;
-  color: ${COLORS.gray700};
-  border: none;
-  border-radius: 8px;
-  background-color: ${COLORS.transparentGray15};
-  appearance: none;
-
-  &:hover {
-    color: ${COLORS.black};
-  }
-`;
-
-const SelectIcon = styled(Icon)`
+const NativeSelect = styled.select`
   position: absolute;
-  top: 9px;
-  right: 4px;
-  color: ${COLORS.gray700};
-  pointer-events: none;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
 
-  ${SelectElement}:hover + & {
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
 `;
 
 export default Select;
